4bf22542567bb7c7ddb01b299c6d82a2a5af0f3a
Devidutta Das <dasdevidutta3@gmail.com>
2026-05-17 13:10:00 +0530

    Add files via upload

---
+++ RandomModule5.py
@@ -0,0 +1,70 @@
+import numpy as np
+"""1.Shuffle:
+[1,2,3,4,5,6,7,8]"""
+def question1():
+    
+ matrix=np.arange(1,9)
+ print(matrix)
+ np.random.shuffle(matrix)
+ print(f"Shuffled Matrix : {matrix}")
+question1()
+"""2.Create a random integer array and shuffle it."""
+import numpy as np
+
+"""2.Create a random integer array and shuffle it."""
+
+def question2():
+    matrix = np.random.randint(
+        low=10,
+        high=51,
+        size=(3,3)
+    )
+    print("Original Matrix:")
+    print(matrix)
+    flatten_matrix = matrix.flatten()
+    np.random.shuffle(flatten_matrix)
+    reshaped_matrix = flatten_matrix.reshape(3,3)
+    print("\nShuffled Matrix:")
+    print(reshaped_matrix)
+
+question2()
+
+"""3.Create a 4×2 matrix and shuffle rows."""
+
+def question3():
+    matrix=np.random.randint(
+        low=1,
+        high=50,
+        size=(4,2)
+    )
+    print("Original Matrix:")
+    print(matrix)
+    flatten_matrix=matrix.flatten()
+    np.random.shuffle(flatten_matrix)
+    reshaped_matrix= flatten_matrix.reshape(4,2)
+    print("\nShuffled Matrix:")
+    print(reshaped_matrix)
+   
+question3()
+
+"""4.Shuffle a list of student names."""
+
+def std_names():
+    std_name_matrix=np.array(["Krishna","Rahul","Rohit","Hari","Gopal"])
+    np.random.shuffle(std_name_matrix)
+    print(f"The Shuffled Name Matrix:{std_name_matrix}")
+    
+std_names()
+
+print("\n---------- Mini Challange ---------- \n")
+def original_playlist():
+    var=np.array(["Song1","Song2","Song3","Song4","Song5"])
+    print(var)
+    return var
+
+def shuffle_playlist():
+   stored_var=original_playlist()
+   shuffled_var = stored_var.copy()
+   np.random.shuffle(shuffled_var)
+   print(f"Shuffled Playlist: {shuffled_var}")
+shuffle_playlist()
---
+++ RandomModuleshuffle.md
@@ -0,0 +1,18 @@
+<h1>Shuffle () In Random Module In Python</h1>
+<p>It is Modifies the original Array or given matrix</p>
+<p>It is also called as In-Place Modification</p>
+<em>Syntax</em>
+<code>np.random.shuffle(array)</code>
+
+<code><em>E.G: 
+    import numpy as np
+    arr=np.aaray([1,2,3,4,5])
+    np.random.shuffle(arr)
+    print(arr)
+
+    <h3><strong>Refere RandomModule5.py for Assignment <br>
+        if anywhere  stuck the contact me </strong></h3>
+        <hr>
+        
+  </em></code>
+  <a href="mailto:dasdevidutta3@://gmail.com">Click Here To Contact></a>
